ed63edb609fe9a8053a37591ea6a8e8fa6abecd5
Anna Valkov <anna.a.valkov@gmail.com>
2014-08-18 16:10:13 -0700

    list all products

--- pos.rb
+++ pos.rb
@@ -31,13 +31,30 @@ def add_menu
 	end
 end
 
+def list_menu
+	puts "Choose and option:"
+	puts "1. List products"
+	case gets.chomp.to_i
+	when 1 then list_products
+	end
+end
+
 def add_product
 	puts "Enter the name of the product:"
 	name = gets.chomp
 	puts "Enter the price of the product:"
 	price = gets.chomp
 	new_product = Product.new(:name => name, :price => price)
+	new_product.save
 	puts new_product.name + " has been added to the system"
 end
+
+def list_products
+	puts "All Products:"
+	products = Product.all.each_with_index do |product, index|
+		puts (index+1).to_s + ". " + product.name + " $" + product.price.to_s
+	end
+end
+
 
 main_menu
